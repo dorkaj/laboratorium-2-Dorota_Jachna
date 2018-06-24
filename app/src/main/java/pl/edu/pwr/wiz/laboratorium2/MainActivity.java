967a42f466e6fbefdffcbb2edee2e5b4467260d1
Dorota Jachna <(dorota.jachna@gmail.com)>
2018-06-24 11:03:07 +0200

    Dorota Jachna labolatorium 2

--- app/src/main/java/pl/edu/pwr/wiz/laboratorium2/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/wiz/laboratorium2/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -15,9 +16,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -26,16 +29,22 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
     private static final int MY_PERMISSIONS_REQUEST_PHONE = 1;
     private static final int MY_PERMISSIONS_REQUEST_CONTACTS = 2;
     private static final int MY_PERMISSIONS_REQUEST_SMS = 3;
+    private static final int MY_PERMISSIONS_READ_EXTERNAL_STORAGE = 4;
 
     private static final int REQUEST_IMAGE_CAPTURE = 0;
     private static final int REQUEST_CONTACT_DATA = 1;
+    private static final int REQUEST_IMAGE_PICK = 2;
 
     private String number;
+
+    private boolean isFastSMSChoosen = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+
+            case R.id.action_gallery:
+                return pickFromGallery();
             case R.id.action_photo:
                 return takePhoto();
 
@@ -101,12 +113,59 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return;
             }
+
+            case MY_PERMISSIONS_REQUEST_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendSMS();
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.access_denied, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            case MY_PERMISSIONS_READ_EXTERNAL_STORAGE: {
+
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickFromGallery();
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.access_denied, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
         }
     }
+
+
 
     /* Callback do rezultatów z różnych wywoływanych aktywności */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && requestCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try{
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                /*Czyścimy aktualny content*/
+
+                ViewGroup contentMain = (ViewGroup) this.findViewById(R.id.content_main);
+                contentMain.removeAllViews();
+                /* Tworzymy nowy obrazek*/
+                ImageView imageView = new ImageView(this);
+                imageView.setImageBitmap(bitmap);
+                imageView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT));
+
+                /*Dodajemy nowy obrazekdo layoutu*/
+
+                contentMain.addView(imageView);
+            }catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+
+
+
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
@@ -118,7 +177,8 @@ public class MainActivity extends AppCompatActivity {
             /* Tworzymy nowy obrazek */
             ImageView imageView = new ImageView(this);
             imageView.setImageBitmap(imageBitmap);
-            imageView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+            imageView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT));
 
             /* Dodajemy nowy obrazek do layoutu */
             contentMain.addView(imageView);
@@ -141,7 +201,8 @@ public class MainActivity extends AppCompatActivity {
                 nameView.setText(name);
 
                 /* Ustawiamy parametry dla widoku */
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
                 nameView.setLayoutParams(params);
 
                 /* Dodajemy nowy tekst do layoutu */
@@ -171,7 +232,7 @@ public class MainActivity extends AppCompatActivity {
                     callBtn.setLayoutParams(params);
 
                     /* Obsluga klikniecia w przycisk */
-                    callBtn.setOnClickListener(new View.OnClickListener() {
+                    callBtn.setOnClickListener(new OnClickListener() {
                         public void onClick(View v) {
                             callNumber();
                         }
@@ -179,7 +240,31 @@ public class MainActivity extends AppCompatActivity {
 
                     /* Dodajemy do layoutu */
                     contentMain.addView(callBtn);
+
+                    Button fastSMSBtn = new Button( this);
+                    fastSMSBtn.setText(R.string.button_fast_sms);
+                    fastSMSBtn.setLayoutParams(params);
+
+                    fastSMSBtn.setOnClickListener(new OnClickListener(){
+                        public void onClick(View view){
+                            isFastSMSChoosen = true;
+                            sendSMS();
+                        }
+
+                    });
+                    contentMain.addView(fastSMSBtn);
+                    Button intentSMSBtn = new Button(this);
+                    intentSMSBtn.setText(R.string.intent_SMSBtn);
+                    intentSMSBtn.setLayoutParams(params);
+                    intentSMSBtn.setOnClickListener(new View.OnClickListener(){
+                        public void onClick(View v){
+                            isFastSMSChoosen = false;
+                        }
+                    });
+                    contentMain.addView(intentSMSBtn);
                 }
+
+
 
             }
         }
@@ -244,4 +329,46 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    //Wysyłanie sms
+    private  boolean sendSMS(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SMS);
+            return false;
+        }
+        if (isFastSMSChoosen){
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(number, null, "Bedę jutro ", null, null);
+        Toast.makeText(getApplicationContext(), "Wiadomość została wysłana", Toast.LENGTH_LONG).show();
+
+        }else{
+            Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+            smsIntent.setType("vnd.android-dir/mms-sms");
+            smsIntent.putExtra("address", number);
+            smsIntent.putExtra("sms_body", "");
+            startActivity(smsIntent);
+        }
+    return true;
+    }
+    private  boolean pickFromGallery(){
+    if(ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+        ActivityCompat.requestPermissions(this, new String[]{
+        Manifest.permission.READ_EXTERNAL_STORAGE},
+                MY_PERMISSIONS_READ_EXTERNAL_STORAGE);
+        return false;
+    }
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (galleryIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(galleryIntent, REQUEST_IMAGE_PICK);
+            return true;
+        }
+
+        return false;
+    }
 }
+
+
